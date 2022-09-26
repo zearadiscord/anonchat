5ff24ccd93a4dfcdbe4d0bdb73a8866302a14d73
zearakun <91083604+zearakun@users.noreply.github.com>
2022-09-26 21:39:07 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBjIe0FwN696PXpd5mWHsB7UCqyzVUHERI",
+    authDomain: "zearadiscord-a603c.firebaseapp.com",
+    projectId: "zearadiscord-a603c",
+    storageBucket: "zearadiscord-a603c.appspot.com",
+    messagingSenderId: "36207211438",
+    appId: "1:36207211438:web:12d5d7c32c59fb73114e3f",
+    measurementId: "G-S71KSYQYTP"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
