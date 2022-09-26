1ff3fbc2ec8cd6a9d97ec6d998b009c251ecf29e
zearakun <91083604+zearakun@users.noreply.github.com>
2022-09-26 21:39:55 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,80 @@
+<?php
+$fp = fopen('data.txt', 'a');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  fputcsv($fp, [$_POST["message"],date("m月d日 H時i分s秒"),"\n"]);
+  rewind($fp);
+};
+fclose($fp);
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title>Anony Chat</title>
+  <style>
+    *{margin: 0;padding: 0;list-style: none}
+    .wrap{
+      width: 600px;
+      margin: 0 auto;
+      padding: 20px 0 100px 0;
+      background: #f1f1f2;
+      min-height: 100vh;
+    }
+    li{
+      position: relative;
+      padding: 10px 20px;
+      margin: 0 10px 10px 10px;
+      background-color: #fff;
+      boeder-radius: 5px;
+    }
+    span{
+      position: absolute;
+      top: 50px;
+      transform: translateY(-50px);
+      right: 10px;
+      font-size: 12px;
+      color: #ccc;
+    }
+    form{
+      position: fixed;
+      top: 10px;
+      left: 5vw;
+    }
+  </style>
+</head>
+<body>
+  <div class="wrap">
+    <ul>
+      
+    </ul>
+  </div>
+  <form action="index.php" method="post">
+    <input type="text" name="message">
+    <button>Send</button>
+  </form>
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+<script>
+  $(function() {
+    $.ajax({
+      url: "data.txt"
+    })
+    .done(function(data) {
+      data.split("\n").forEach(function(chat) {
+        const post_text = chat.split(",")[0];
+        const post_time = chat.split(",")[1];
+        if(post_text) {
+          const = li = `<li>${post_text}<span>${post_time}</span></li>`;
+          $("ul").append(li)
+        }
+      });
+    });
+  });
+</script>
+</body>
+</html>
+
 <script type="module">
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
